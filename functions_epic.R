f218ae89d281fac710fea94017f63c82b81d8a00
nickgood <n.good@colostate.edu>
2021-11-04 10:05:17 -0600

    remove old omni read function

--- functions_epic.R
+++ functions_epic.R
@@ -27,44 +27,6 @@ clean_names <- function(x){
 #_______________________________________________________________________________
 
 #_______________________________________________________________________________
-# read 5 minute omni data
-d_5min <- function(date_start, date_end,
-                   tkn = "",
-                   org_id = "2059",
-                   device_type = "awair-omni",
-                   device_id = "11431", ...){
-# pause
- Sys.sleep(2)
-# url  
-  url <- paste0("http://developer-apis.awair.is/v1/orgs/",
-                org_id, "/",
-                "devices/", device_type, "/", device_id, "/",
-                "air-data/5-min-avg?",
-                "from=", date_start,
-                "&to=", date_end)
-# GET data
-  result <- GET(url, add_headers(Authorization = paste("Bearer", tkn, sep = " "))) %>%
-    content() %>%
-    as_tibble()
-
-if(length(result$data) > 0){
-# clean data
-  out <- as_tibble(result) %>%
-  unnest_wider(data) %>%
-  select(-indices) %>%
-  mutate(df = map(sensors, ~ .x %>% map_df(magrittr::extract, c("comp", "value")))) %>%
-  unnest(df) %>%
-  select(-sensors) %>%
-  pivot_wider(names_from = "comp", values_from = "value") %>%
-  mutate(datetime_local = format(as.POSIXct(strptime(timestamp,format = "%FT%H:%M:%S",tz = "GMT")),
-                           tz = "US/Mountain",
-                           usetz = TRUE))
-}else{NA}
-
-}
-#_______________________________________________________________________________
-
-#_______________________________________________________________________________
 # upas time to posixct
 upas_to_datetime <- function(x){
   as.POSIXct(strptime(sub("T"," ", x),
